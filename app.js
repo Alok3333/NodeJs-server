b46aba3914161ea3e7037aff9c2ca053d796c64e
Alok3333 <alokprajapati697@gmail.com>
2024-06-08 11:42:55 +0530

    created a routes / and /currencies to get api data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require express.js
+const currenciesData = require("./api_data.json");
 const express = require("express");
 
 // Instance of expressJs
@@ -7,7 +7,12 @@ const PORT = 8082
 
 // create a routes get
 app.get("/", (req, res) => {
-    res.send({"serverStatus": "active"});
+    res.send(`<h1>Currency Database</h1>`);
+});
+
+// /currencies get route
+app.get("/currencies", (req, res) => {
+    res.send(currenciesData.data);
 });
 
 // listening a port
