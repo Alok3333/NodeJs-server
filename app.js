f908fe60564396b2c7ec8beb0f75a1a9894975ff
Alok3333 <alokprajapati697@gmail.com>
2024-06-09 12:24:21 +0530

    created a folder controllers and make less code in app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const currenciesData = require("./api_data.json");
+const { getCurrencies, getCurrenciesBySymbol } = require("./controllers/controllers.currencies");
 const express = require("express");
 
 // Instance of expressJs
@@ -11,25 +11,9 @@ app.get("/", (req, res) => {
 });
 
 // /currencies get route
-app.get("/currencies", (req, res) => {
-    const { min_value } = req.query;
-    // console.log(typeof (min_value), min_value, "min val");
-    if(min_value){
-        const currenciesDataWithFilter = currenciesData.data.filter(({min_size}) => Number(min_size) === Number(min_value));
-        return res.status(200).send(currenciesDataWithFilter);
-    }
-    
-    res.status(200).send(currenciesData.data);
-});
+app.get("/currencies", getCurrencies);
 
-app.get("/currencies/:symbol", (req, res) => {
-    const currenciesDataFindWithSymbol = currenciesData.data.find((currency) => currency.id.toLowerCase() === req.params.symbol.toLowerCase());
-    // console.log(typeof(currenciesDataFindWithSymbol))
-    if (currenciesDataFindWithSymbol === undefined)
-        return res.status(404).send({ message: "Invalid Symbol" });
-    res.status(200).send(currenciesDataFindWithSymbol);
-    console.log(currenciesDataFindWithSymbol)
-});
+app.get("/currencies/:symbol", getCurrenciesBySymbol);
 
 
 // listening a port
