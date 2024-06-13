ce86fc065bafc503b4a64282c9e44ee754d41877
Alok3333 <alokprajapati697@gmail.com>
2024-06-13 20:26:35 +0530

    modify getUserSearch function by gender and age

--- node-commonJs/controllers/users.controllers.js
+++ node-commonJs/controllers/users.controllers.js
@@ -11,19 +11,28 @@ const getUsers = async (req, res) => {
 
 const getUserByUuid = async (req, res) => {
     const { uuid } = req.params;
-    console.log(uuid);
+    // console.log(uuid);
     try {
         const { data: usersData } = await axios.get(`https://gitlab.crio.do/public_content/node-js-sessions/-/raw/master/users.json`);
         if (uuid)
-            return res.send(usersData.data.filter((user) => user.login.uuid === uuid));
+            return res.send(usersData.data.find((user) => user.login.uuid === uuid));
         res.status(400).send({ message: "Bad Request" });
     } catch (error) {
-        res.status(500).send({ message: "server not responsed!" })
+        res.status(500).send({ message: "server not responsed!" });
     }
 };
 
 const getUserSearchByGenderAndAge = async (req, res) => {
-    console.log(req.query, "search by user male or female");
+    const { gender, age } = req.query;
+    console.log(req.query);
+    try {
+        const { data: usersData } = await axios.get(`https://gitlab.crio.do/public_content/node-js-sessions/-/raw/master/users.json`);
+        if (gender && age)
+            return res.send(usersData.data.filter((user) => user.gender === gender && user.dob.age === parseInt(age)));
+        res.status(400).send({ message: "Please provides gender or age must to be pass both of them" });
+    } catch (error) {
+        res.status(500).send({ message: "url query are not passed." });
+    }
 };
 
 module.exports = { getUsers, getUserByUuid, getUserSearchByGenderAndAge }
